12f07861077946151e0f46970e6028de93c3e03b
vaaishalijain <vaishalij00@gmail.com>
2017-04-21 18:23:57 +0530

    signup.php

---
+++ website/signup.php
@@ -0,0 +1,22 @@
+<html>
+<title>
+New User
+</title>
+<body>
+<form name="register" action="login.php" method="post">
+<fieldset cols="30">
+<p id="demo"></p>
+<pre><strong>First Name</strong> <input type="text" name="fname" required><br><br></pre>
+<pre><strong>Last Name</strong>  <input type="text" name="lname"><br><br></pre>
+<pre><strong>Email id</strong>   <input type="email" name="mail" required><br><br></pre>
+<pre><strong>User Name</strong>  <input type="text" name="userid" required><br><br></pre>
+<pre><strong>Password</strong>   <input type="password" name="pass" placeholder="Atleast 6 char" required id="passid" size="10"><br><br></pre>
+<pre><strong>Mobile</strong>     <input type="text" name="num" required size="10"><br><br></pre>
+<input type="submit" value="Create Account"></button>
+<legend>
+New User
+</legend>
+</fieldset>
+</form>
+</body>
+</html>
